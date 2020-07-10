4d345f3d17a87f0048228b3a27ff16531b0bee9f
Marcelo Reyna <lepozepo@gmail.com>
2020-07-10 14:28:15 -0700

    Lint weirdness

--- apolloschurchapp/src/LandingScreen.js
+++ apolloschurchapp/src/LandingScreen.js
@@ -4,16 +4,15 @@ import { styled, withTheme } from '@apollosproject/ui-kit';
 
 import ApollosLandingScreen from './ui/LandingScreen';
 
-// BackgroundComponent={
-//   <FullScreenImage source={require('./ui/LandingScreen/background.png')} />
-// }
-
 const LandingScreen = withTheme(({ theme }) => ({
   textColor: theme.colors.text.primary,
 }))(({ navigation, textColor }) => (
   <ApollosLandingScreen
     onPressPrimary={() => navigation.push('Auth')}
     textColor={textColor}
+    // BackgroundComponent={
+    //   <FullScreenImage source={require('./ui/LandingScreen/background.png')} />
+    // }
     primaryNavText={"Let's go!"}
   />
 ));
