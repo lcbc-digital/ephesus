b2b53a1d3cd1fcb626a1a98b13ac6f8ed655540e
Vincent Wilson <vince@classyh.at>
2020-07-13 15:56:27 -0400

    User campus feature

--- apollos-church-api/src/data/Feature.js
+++ apollos-church-api/src/data/Feature.js
@@ -1,11 +1,27 @@
 import { Feature } from '@apollosproject/data-connector-rock';
-import { createGlobalId } from '@apollosproject/server-core';
+import { createGlobalId, parseGlobalId } from '@apollosproject/server-core';
 import { get } from 'lodash';
+import gql from 'graphql-tag';
 
-const { resolver: baseResolver, schema } = Feature;
+const { resolver: baseResolver, schema: baseSchema } = Feature;
+
+const schema = gql`
+  ${baseSchema}
+
+  extend type Query {
+    userFeedFeaturesWithCampus(campusId: ID): [Feature]
+  }
+`;
 
 const resolver = {
   ...baseResolver,
+  Query: {
+    ...baseResolver.Query,
+    userFeedFeaturesWithCampus: (root, { campusId }, context, ...args) => {
+      context.campusId = campusId;
+      return baseResolver.Query.userFeedFeatures(root, null, context, ...args);
+    },
+  },
   ActionListAction: {
     ...baseResolver.ActionListAction,
     subtitle: ({ subtitle, summary }) => subtitle || summary,
@@ -17,8 +33,26 @@ class dataSource extends Feature.dataSource {
     // We need to make sure `this` refers to the class, not the `ACTION_ALGORITHIMS` object.
     ...this.ACTION_ALGORITHIMS,
     MOST_RECENT_SERMON: this.mostRecentSermonAlgorithm.bind(this),
-    SECTION_FEATURE: this.sectionFeature.bind(this),
+    SECTION: this.sectionFeature.bind(this),
+    CAMPUS: this.campusFeature.bind(this),
   };
+
+  getFromId(args, id) {
+    const type = id.split(':')[0];
+    const funcArgs = JSON.parse(args);
+    const method = this[`create${type}`].bind(this);
+    if (funcArgs.campusId) {
+      this.context.campusId = funcArgs.campusId;
+    }
+    return method(funcArgs);
+  }
+
+  createFeatureId({ args, type }) {
+    return createGlobalId(
+      JSON.stringify({ campusId: this.context.campusId, ...args }),
+      type
+    );
+  }
 
   async sectionFeature({ section }) {
     const { ContentItem } = this.context.dataSources;
@@ -30,6 +64,28 @@ class dataSource extends Feature.dataSource {
       title: item.title,
       subtitle: get(item, 'parent.title'),
       relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
+  async campusFeature() {
+    const { ContentItem } = this.context.dataSources;
+
+    if (!this.context.campusId) return [];
+
+    const rockCampusId = parseGlobalId(this.context.campusId).id;
+
+    const items = await ContentItem.getByCampusId({
+      campusId: rockCampusId,
+    });
+
+    return items.map((item, i) => ({
+      id: createGlobalId(`${item.id}${i}`, 'ActionListAction'),
+      title: item.title,
+      subtitle: get(item, 'parent.title'),
+      relatedNode: { ...item, __typename: ContentItem.resolveType(item) },
       image: ContentItem.getCoverImage(item),
       action: 'READ_CONTENT',
       summary: ContentItem.createSummary(item),
