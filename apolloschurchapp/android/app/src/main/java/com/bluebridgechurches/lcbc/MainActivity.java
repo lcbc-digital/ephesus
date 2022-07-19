45a74589b4e15f4b1e53c420c8d8853ff8764e34
nlewis84 <nathan@differential.com>
2022-07-19 08:58:08 -0500

    refactor: move the change up the file

--- apolloschurchapp/android/app/src/main/java/com/bluebridgechurches/lcbc/MainActivity.java
+++ apolloschurchapp/android/app/src/main/java/com/bluebridgechurches/lcbc/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.style.SplashScreenTheme);
-        super.onCreate(savedInstanceState);
+        super.onCreate(null);
     }
 
     @Override
@@ -31,9 +31,4 @@ public class MainActivity extends ReactActivity {
         }
       };
     }
-
-    @Override
-      protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-}
 }
