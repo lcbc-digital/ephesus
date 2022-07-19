d1b7ca460c8dbb5980f545dc6f9d9b43e1808197
Nathan Lewis <nathan@differential.com>
2022-07-19 09:41:03 -0500

    fix: samsung crash fix (#80)
    * fix: samsung crash fix

--- apolloschurchapp/android/app/src/main/java/com/bluebridgechurches/lcbc/MainActivity.java
+++ apolloschurchapp/android/app/src/main/java/com/bluebridgechurches/lcbc/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.style.SplashScreenTheme);
-        super.onCreate(savedInstanceState);
+        super.onCreate(null);
     }
 
     @Override
